b0563ef6e3e46c090b83d760d81b97dcb68867e9
hienlehitek <hienle@hitek.com.vn>
2024-06-03 20:42:48 +0700

    get post by id (custom)

--- src/main/java/com/ismhac/jspace/service/PostService.java
+++ src/main/java/com/ismhac/jspace/service/PostService.java
@@ -5,11 +5,12 @@ import com.ismhac.jspace.dto.post.response.PostDto;
 import com.ismhac.jspace.model.enums.*;
 import org.springframework.data.domain.Pageable;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public interface PostService {
 
-    PostDto getById(int id);
+    LinkedHashMap<String, Object> getById(int id, Integer candidateId);
 
     PageResponse<Map<String, Object>> getAllAndFilter(Integer candidateId, Experience experience, Gender gender, JobType jobType, Location location, Rank rank, Integer quantity, String title, String companyName, Integer maxPay, Integer minPay, Pageable pageable);
 }
