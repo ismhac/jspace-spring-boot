771f68d4e8af0aa04d88939f9d3ddba2b7185709
hienlehitek <hienle@hitek.com.vn>
2024-07-08 20:45:20 +0700

    handle dashboard

--- src/main/java/com/ismhac/jspace/controller/dashboard/DashboardController.java
+++ src/main/java/com/ismhac/jspace/controller/dashboard/DashboardController.java
@@ -4,10 +4,8 @@ import com.ismhac.jspace.dto.common.response.ApiResponse;
 import com.ismhac.jspace.service.dashboard.AdminDashboardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Map;
 
 @RestController
@@ -17,6 +15,9 @@ import java.util.Map;
 public class DashboardController {
     private final AdminDashboardService adminDashboardService;
 
+    /* ADMIN  */
+
+    /* user */
     @GetMapping("/admins/users/statistic-month")
     public ApiResponse<Map<Integer, Integer>> userStatisticByMonth(@RequestParam("year") int year, @RequestParam("month") int month){
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getUserCountsForMonth(year, month)).build();
@@ -27,6 +28,7 @@ public class DashboardController {
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getUserCountsForYear(year)).build();
     }
 
+    /* post */
     @GetMapping("/admins/posts/statistic-month")
     public ApiResponse<Map<Integer, Integer>> postStatisticByMonth(@RequestParam("year") int year, @RequestParam("month") int month){
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getPostCountsForMonth(year, month)).build();
@@ -37,6 +39,7 @@ public class DashboardController {
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getPostCountsForYear(year)).build();
     }
 
+    /* company */
     @GetMapping("/admins/companies/statistic-month")
     public ApiResponse<Map<Integer, Integer>> companyStatisticByMonth(@RequestParam("year") int year, @RequestParam("month") int month){
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getCompanyCountsForMonth(year, month)).build();
@@ -47,6 +50,14 @@ public class DashboardController {
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getCompanyCountsForYear(year)).build();
     }
 
+    @GetMapping("/admins/statistics")
+    public ApiResponse<Object> adminDashboardStatistic(){
+        return ApiResponse.<Object>builder().result(adminDashboardService.adminDashboardStatistic()).build();
+    }
+
+
+    /* EMPLOYEE */
+
     @GetMapping("/companies/posts/statistic-month")
     public ApiResponse<Map<Integer, Integer>> postStatisticByMonthOfCompany(@RequestParam("year") int year, @RequestParam("month") int month, @RequestParam("companyId") int companyId){
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getPostCountsForMonthOfCompany(year, month, companyId)).build();
@@ -55,5 +66,10 @@ public class DashboardController {
     @GetMapping("/companies/posts/statistic-year")
     public ApiResponse<Map<Integer, Integer>> postStatisticByYearOfCompany(@RequestParam("year") int year, @RequestParam("companyId") int companyId){
         return ApiResponse.<Map<Integer, Integer>>builder().result(adminDashboardService.getPostCountsForYearOfCompany(year, companyId)).build();
+    }
+
+    @GetMapping("/companies/{companyId}/statistic")
+    public ApiResponse<Object> companyStatistic(@PathVariable("companyId") int companyId){
+        return ApiResponse.builder().result(adminDashboardService.companyStatistic(companyId)).build();
     }
 }
