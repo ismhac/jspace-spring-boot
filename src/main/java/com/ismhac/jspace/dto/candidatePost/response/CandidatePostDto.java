0fb821c9f69cc8982277ae8de92b5522b55d192b
hienlehitek <hienle@hitek.com.vn>
2024-07-18 22:59:39 +0700

    add date-time info

--- src/main/java/com/ismhac/jspace/dto/candidatePost/response/CandidatePostDto.java
+++ src/main/java/com/ismhac/jspace/dto/candidatePost/response/CandidatePostDto.java
@@ -1,10 +1,13 @@
 package com.ismhac.jspace.dto.candidatePost.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ismhac.jspace.dto.other.ApplyStatusDto;
 import com.ismhac.jspace.dto.post.response.PostDto;
 import com.ismhac.jspace.dto.resume.response.ResumeDto;
 import com.ismhac.jspace.dto.user.candidate.response.CandidateDto;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Data
 public class CandidatePostDto {
@@ -13,4 +16,6 @@ public class CandidatePostDto {
     ResumeDto resume;
     ApplyStatusDto applyStatus;
     String note;
+    @JsonProperty("updatedTime")
+    Instant updatedAt;
 }
